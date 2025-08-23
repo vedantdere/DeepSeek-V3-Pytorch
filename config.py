47a44d48ede21c81dfb52fe8b848b60687139234
vedantdere <derevedant@gmail.com>
2025-08-23 10:03:18 +0530

    added config

---
+++ config.py
@@ -0,0 +1,103 @@
+from transformers.configuration_utils import PretrainedConfig
+from transformers.modeling_rope_utils import rope_config_validation
+
+class DeepseekV3Config(PretrainedConfig):
+    model_type = "deepseek_v3"
+    keys_to_ignore_at_inference = ["past_key_values"]
+
+    
+    def __init__(
+                 self,
+                 vocab_size=129280,
+                 hidden_size=7168,
+                 intermediate_size=18432,
+                 moe_intermediate_size=2048,
+                 num_hidden_layers=61,
+                 num_attention_heads=128,
+                 num_key_value_heads=128,
+                 n_routed_experts=1,
+                 n_shared_experts=256,
+                 routed_scaling_factor=2.5,
+                 kn_lora_rank=512,
+                 q_lora_rank=1536,
+                 qk_rope_head_dim=64,
+                 v_head_dim=128,
+                 qk_nope_head_dim=128,
+                 n_group=8,
+                 topk_group=4,
+                 num_experts_per_tok=8,
+                 first_k_dense_replace=3,
+                 norm_tokp_prob=True,
+                 hidden_act='silu',
+                 max_position_embeddings=4096,
+                 initializer_range=0.02,
+                 rms_norm_eps=1e-6,
+                 use_cache=True,
+                 pad_token_id=None,
+                 bos_token_id=0,
+                 eos_token_id=1,
+                 tie_word_embeddings=False,
+                 rope_theta=10000.0,
+                 rope_scaling=None,
+                 rope_interleave=True,
+                 attention_bias=False,
+                 attention_dropout=0.0,
+                 **kwargs
+                 ):
+        
+        self.vocab_size = vocab_size
+        self.max_position_embeddings = max_position_embeddings
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.moe_intermediate_size = moe_intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.n_shared_experts = n_shared_experts
+        self.n_routed_experts = n_routed_experts
+        self.routed_scaling_factor = routed_scaling_factor
+        self.kv_lora_rank = kn_lora_rank
+
+        self.q_lora_rank = q_lora_rank
+        self.qk_rope_head_dim = qk_nope_head_dim
+        self.v_head_dim = v_head_dim
+        self.qk_nopre_head_dim = qk_nope_head_dim
+        self.qk_head_dim = qk_nope_head_dim + qk_rope_head_dim
+        self.head_dim = qk_rope_head_dim
+        self.n_group = n_group
+        self.topk_group = topk_group
+        self.num_experts_per_tok = num_experts_per_tok
+        self.first_k_dense_replace = first_k_dense_replace
+        self.norm_top_prob = norm_tokp_prob
+        self.rope_interleave = rope_interleave
+
+        if num_key_value_heads is None:
+            num_key_value_heads = num_attention_heads
+
+        self.num_key_value_heads = num_key_value_heads
+        self.hidden_act = hidden_act
+        self.initializer_range = initializer_range
+        self.rms_norm_eps = rms_norm_eps
+        self.use_cache = use_cache
+        self.rope_theta = rope_theta
+        self.rope_scaling = rope_scaling
+        self.attention_bias = attention_bias
+        self.attention_dropout = attention_dropout
+
+        if self.rope_scaling is not None and "type" is self.rope_scaling:
+            self.rope_scaling["rope_type"] = self.rope_scaling["type"]
+        
+        if self.rope_scaling is not None:
+            for key in ["beta_fast", "beta_slow","factor"]:
+                if key in self.rope_scaling:
+                    self.rope_scaling[key] = float(self.rope_scaling[key])
+        
+        rope_config_validation(self)
+
+        super().__init__(
+            pad_token_id=pad_token_id,
+            bos_token_id=bos_token_id,
+            eos_token_id=eos_token_id,
+            tie_word_embeddings=tie_word_embeddings,
+            **kwargs
+        )
+
